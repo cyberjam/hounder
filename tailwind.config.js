b48c94e7e66a1048dd4919597eb6bebb1f8cea60
cyberjam <jamsince1994@naver.com>
2023-04-20 16:07:23 +0900

    CHORE: css lint prettier 설정

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      spacing: {
+        128: "32rem",
+      },
+      colors: {
+        yellow: "#ffd149",
+        orange: "#ffa000",
+        brown: "#c67100",
+      },
+    },
+  },
+  plugins: [],
+};
